04313c043fc8e6e007539bc58be3b16fa388d696
Matyi Fekete <creak93@gmail.com>
2019-03-20 09:30:28 +0100

    init spring project

---
+++ src/main/java/training360/j5webshop/J5webshopApplication.java
@@ -0,0 +1,13 @@
+package training360.j5webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J5webshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J5webshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/training360/j5webshop/J5webshopApplicationTests.java
@@ -0,0 +1,16 @@
+package training360.j5webshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class J5webshopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
