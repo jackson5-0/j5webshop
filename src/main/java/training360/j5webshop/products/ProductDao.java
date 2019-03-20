e16c0dc2951fde0cffb7896a4519a9c723c786d1
Kerklecz Médea <kerklecz.medea@gmail.com>
2019-03-20 21:51:02 +0100

    új createProduct a DAO-ban

--- src/main/java/training360/j5webshop/products/ProductDao.java
+++ src/main/java/training360/j5webshop/products/ProductDao.java
@@ -1,12 +1,14 @@
 package training360.j5webshop.products;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 @Repository
 public class ProductDao {
@@ -45,10 +47,27 @@ public class ProductDao {
         });
     }
 
-    public void createProduct(Product product){
-        jdbcTemplate.update("insert into product (code, name, address, publisher, price) values(?, ?, ?, ?, ?)",
-                product.getCode(), product.getName(), product.getAddress(), product.getPublisher(), product.getPrice());
-    }
+//    public void createProduct(Product product){
+//        jdbcTemplate.update("insert into product (code, name, address, publisher, price) values(?, ?, ?, ?, ?)",
+//                product.getCode(), product.getName(), product.getAddress(), product.getPublisher(), product.getPrice());
+        public long createProduct(Product product){
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+            jdbcTemplate.update(new PreparedStatementCreator() {
+                @Override
+                public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                    PreparedStatement ps = connection.prepareStatement
+                            ("insert into product (code, name, address, publisher, price) values(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+                    ps.setString(1, product.getCode());
+                    ps.setString(2, product.getName());
+                    ps.setString(3, product.getAddress());
+                    ps.setString(4, product.getPublisher());
+                    ps.setInt(5, product.getPrice());
+                    return ps;
+                    }
+                }, keyHolder
+            );
+            return keyHolder.getKey().longValue();
+        }
 
     public Product findProductByAddress(String address) {
         return jdbcTemplate.queryForObject("select code, name, address, publisher, price from product where address = ?",
