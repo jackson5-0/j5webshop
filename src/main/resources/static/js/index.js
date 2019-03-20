58a82a98ca7fc7587b863f83bd84127a1f4a1c8b
Matyi Fekete <creak93@gmail.com>
2019-03-20 15:24:02 +0100

    index.html, index.js (list products with page navigation)

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,61 @@
+fetchProducts();
+
+function fetchProducts() {
+  fetchPageNavigators();
+  var pageNum = (new URL(document.location)).searchParams.get('page');
+  var start = (parseInt(pageNum, 10) - 1) * 10;
+  var url = `/products?start=${start || 0}&size=${10}`;
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (jsonData) {
+      showProducts(jsonData);
+    });
+}
+
+function fetchPageNavigators() {
+  fetch('products/count')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (jsonData) {
+      showPageNavigator(jsonData);
+    });
+}
+
+function showPageNavigator(jsonData) {
+  var pages = document.getElementById('page-number');
+  var numberOfPages = Math.ceil(jsonData / 10);
+  for (var i = 1; i <= numberOfPages && i < 10; i++) {
+    pages.innerHTML += `<a href="index.html?page=${i}">${i}</a>`;
+  }
+}
+
+function showProducts(jsonData) {
+  var allProducts = document.getElementById('products');
+  allProducts.innerHTML = '';
+  for (var i = 0; i < jsonData.length; i++) {
+    var product = document.createElement('div');
+
+    var code = document.createElement('span');
+    var name = document.createElement('span');
+    var address = document.createElement('span');
+    var publisher = document.createElement('span');
+    var price = document.createElement('span');
+
+    code.innerHTML = jsonData[i].code;
+    name.innerHTML = jsonData[i].name;
+    address.innerHTML = jsonData[i].address;
+    publisher.innerHTML = jsonData[i].publisher;
+    price.innerHTML = jsonData[i].price;
+
+    product.appendChild(code);
+    product.appendChild(name);
+    product.appendChild(address);
+    product.appendChild(publisher);
+    product.appendChild(price);
+
+    allProducts.appendChild(product);
+  }
+}
