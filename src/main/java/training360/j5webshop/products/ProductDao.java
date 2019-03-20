1f7ae05873e5f5ed1d7c26782fa7f26ff7786204
Kovács Dániel <kovacsdaniel076@gmail.com>
2019-03-20 11:28:28 +0100

    listProductsWithLimit és listAllProducts metódusok implementálása (nincs tesztelve)

--- src/main/java/training360/j5webshop/products/ProductDao.java
+++ src/main/java/training360/j5webshop/products/ProductDao.java
@@ -1,8 +1,12 @@
 package training360.j5webshop.products;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 public class ProductDao {
     private JdbcTemplate jdbcTemplate;
@@ -11,10 +15,23 @@ public class ProductDao {
         jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public Product findProductById(long id) {
-        return jdbcTemplate.queryForObject("select code, name, address, publisher, price from j5webshop where id = ?",
-                (rs, rowNum) -> new Product(rs.getString("code"), rs.getString("name"), rs.getString("address"),
-                        rs.getString("publisher"), rs.getInt("price")),
-                id);
+    public List<Product> listProductsWithLimit(int start,int size) {
+        return jdbcTemplate.query("select code, name, address, publisher, price from j5webshop where rownum > ? limit ?", new RowMapper<Product>() {
+            @Override
+            public Product mapRow(ResultSet resultSet, int i) throws SQLException {
+                return new Product(resultSet.getString("code"), resultSet.getString("name"),
+                        resultSet.getString("address"), resultSet.getString("publisher"), resultSet.getInt("price"));
+            }
+        }, start, size);
+    }
+
+    public List<Product> listAllProducts() {
+        return jdbcTemplate.query("select code, name, address, publisher, price from j5webshop", new RowMapper<Product>() {
+            @Override
+            public Product mapRow(ResultSet resultSet, int i) throws SQLException {
+                return new Product(resultSet.getString("code"), resultSet.getString("name"),
+                        resultSet.getString("address"), resultSet.getString("publisher"), resultSet.getInt("price"));
+            }
+        });
     }
 }
