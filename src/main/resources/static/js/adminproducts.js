487654a8552efeb032a59cf3c5aa7f85a84686c1
csorbarenata <csorba.renata@gmail.com>
2019-03-21 10:30:36 +0100

    update nem működik (js)

--- src/main/resources/static/js/adminproducts.js
+++ src/main/resources/static/js/adminproducts.js
@@ -35,34 +35,34 @@ function showPageNavigator(jsonData) {
 
 function showProducts(jsonData) {
     var tableBody = document.getElementById('products-tablebody');
-    tablebody.innerHTML = "";
+    tableBody.innerHTML = "";
     for(var i = 0; i < jsonData.length; i++) {
         var tr = document.createElement('tr');
         tr["raw-data"] = jsonData[i]; //ez eredetileg a delete-button raw-datája volt, ha így nem működne...
 
         var codeTd = document.createElement('td');
         codeTd.contentEditable = "true";
-        codeTd.onchange = updateProduct();
+//        codeTd.onchange = updateProduct();
         codeTd.innerHTML = jsonData[i].code;
 
         var nameTd = document.createElement('td');
         nameTd.contentEditable = "true";
-        nameTd.onchange = updateProduct();
+//        nameTd.onchange = updateProduct();
         nameTd.innerHTML = jsonData[i].name;
 
         var addressTd = document.createElement('td');
         addressTd.contentEditable = "true";
-        addressTd.onchange = updateProduct();
+//        addressTd.onchange = updateProduct();
         addressTd.innerHTML = jsonData[i].address;
 
         var publisherTd = document.createElement('td');
         publisherTd.contentEditable = "true";
-        publisherTd.onchange = updateProduct();
+//        publisherTd.onchange = updateProduct();
         publisherTd.innerHTML = jsonData[i].publisher;
 
         var priceTd = document.createElement('td');
         priceTd.contentEditable = "true";
-        priceTd.onchange = updateProduct();
+//        priceTd.onchange = updateProduct();
         priceTd.innerHTML = jsonData[i].price;
 
         var statusTd = document.createElement('td');
@@ -72,6 +72,7 @@ function showProducts(jsonData) {
         var deleteButton = document.createElement('button');
         deleteButton.innerHTML = "Termék törlése";
         deleteButton.onclick = deleteProduct;
+        buttonTd["raw-data"] = jsonData[i].id;
         buttonTd.appendChild(deleteButton);
 
         tr.appendChild(nameTd);
@@ -123,44 +124,47 @@ function deleteProduct() {
         return;
     }
     fetch("/api/products/" + id, {
-        method: "POST"      //vagy mégis DELETE? Akkor nem kell a köv 4 sor!
+        method: "POST",      //vagy mégis DELETE? Akkor nem kell a köv 4 sor!
         body: JSON.stringify(request),
         headers: {
            "Content-type": "application/json"
         }
     })
-    .then(function(){       //ha DELETE, akkor function(response)
-        document.getElementById("message-div").setAttribute("class", "alert alert-success");
-        document.getElementById("message-div").innerHTML = "A terméket sikeresen törölted!";
-        fetchProducts();
+    .then(function(response){
+            return response.json();
+    }).then(function(jsonData) {
+             if (jsonData.status == 'SUCCESS') {
+                 document.getElementById("message-div").setAttribute("class", "alert alert-success");
+                 document.getElementById("message-div").innerHTML = jsonData.message;
+                 fetchProducts();
+             }
     });
 }
 
-function updateProduct() {
-    var id = this["raw-data"].id;           //vagy =(new URL(document.location)).searchParams.get("id");
-//    var code = ?
-//    var name = ?
-//    var name = ?
-//    var name = ?
-    var request = {
-            "code": code,
-            "name": name,
-            "address": address,
-            "publisher": publisher,
-            "price": price,
-            "status": status,   //a státuszt is átállítja az update? vagy ehhez van külön metódus?
-    };
-
-    fetch("/api/products/" + id, {
-        method: "POST",
-        body: JSON.stringify(request),
-        headers: {
-            "Content-type": "application/json"
-        }
-    })
-    .then(function(response) {
-        document.getElementById("message-div").innerHTML = "A termék módosult!"; //vagy return response.json(); ?
-    });
-    return false;
-}
-*/
+//function updateProduct() {
+//    var id = this["raw-data"].id;           //vagy =(new URL(document.location)).searchParams.get("id");
+//    var code = this["raw-data"].code;
+//    var name = this["raw-data"].name;
+//    var address = this["raw-data"].address;
+//    var publisher = this["raw-data"].publisher;
+//    var price = this["raw-data"].price;
+//    var request = {
+//            "code": code,
+//            "name": name,
+//            "address": address,
+//            "publisher": publisher,
+//            "price": price,
+//    };
+//
+//    fetch("/api/products/" + id, {
+//        method: "POST",
+//        body: JSON.stringify(request),
+//        headers: {
+//            "Content-type": "application/json"
+//        }
+//    })
+//    .then(function(response) {
+//        document.getElementById("message-div").innerHTML = "A termék módosult!"; //vagy return response.json(); ?
+//    });
+//    return false;
+//}
