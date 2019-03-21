7c3e518ebf46f1e56394c3ee31752dc7a58972da
Matyi Fekete <creak93@gmail.com>
2019-03-21 17:53:12 +0100

    add user_admin

---
+++ src/main/resources/db/migration/V4__init_db.sql
@@ -0,0 +1,2 @@
+insert into users(firstname, lastname, username, password, enabled, role)
+values ('Kiss', 'Pista', 'admin', '$2a$10$M7/F8ur785gtN51RU.VdiOW/m5/R8HFGiYp9mEC3DjVHZE.PuNK3G', 1, 'ROLE_ADMIN');
