7843a24d63f45560ca700d0c94daf126734fb7f9
Kerklecz Médea <kerklecz.medea@gmail.com>
2019-03-21 13:07:41 +0100

    Order/OrderedProduct osztályok létrehozása

---
+++ src/main/java/training360/j5webshop/orders/OrderedProduct.java
@@ -0,0 +1,11 @@
+package training360.j5webshop.orders;
+
+import training360.j5webshop.products.Product;
+
+public class OrderedProduct {
+
+    private Product product;
+    private int quantity;
+    private int priceAtPurchase;
+
+}
