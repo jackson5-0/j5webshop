9955e1608d597517ca83589c31273c2b105035a1
Matyi Fekete <creak93@gmail.com>
2019-03-22 20:04:29 +0100

    navigation bar depending on user_role

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -19,16 +19,64 @@ function fetchUser() {
       return response.json();
     })
     .then(function (jsonData) {
-      console.log(jsonData);
       var userRole = jsonData.userRole;
       if (userRole == "ROLE_ADMIN") {
-        console.log("Admin");
+        createAdminHeader(jsonData);
       } else if (userRole == "ROLE_USER") {
-        console.log("User");
+        createUserHeader(jsonData);
       } else {
-        console.log("anyad");
+        createDefaultHeader(jsonData);
       }
       user = jsonData;
 
     });
+}
+
+function createAdminHeader(admin) {
+  var nav = document.querySelector(".nav");
+  nav.innerHTML = `
+  <div id="header-top">
+    <a href="/logout">logout</a>
+  </div>
+  <div>
+    <ul>
+      <li><a href="adminproducts.html">Products</a></li>
+      <li><a href="manageusers.html">Users</a></li>
+    </ul>
+  </div>
+  `;
+}
+
+function createDefaultHeader(user) {
+  var nav = document.querySelector(".nav");
+  nav.innerHTML =
+    `<div id="header-top">
+    <a href="register.html">register</a>
+    <a href="/login">login</a>
+  </div>
+  <div>
+    <ul>
+      <li><a href="">Menu1</a></li>
+      <li><a href="">Menu2</a></li>
+      <li><a href="">Menu3</a></li>
+    </ul>
+  </div>`;
+}
+
+function createUserHeader(user) {
+  console.log(user);
+  var nav = document.querySelector(".nav");
+  nav.innerHTML =
+    `<div id="header-top">
+    <span>Hello ${user.userName}</span>
+    <a href="/logout">logout</a>
+    <a href="/basket.html?basket=${user.basketId}"><img src="/img/cart.png"></a>
+  </div>
+  <div>
+    <ul>
+      <li><a href="">Menu1</a></li>
+      <li><a href="">Menu2</a></li>
+      <li><a href="">Menu3</a></li>
+    </ul>
+  </div>`;
 }
