0aa3b52840d7cd30ac987ee4305152ba72a9b222
GMiki <gmiki91@gmail.com>
2019-03-22 17:29:26 +0100

    navigatbar/role template

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,11 +1,34 @@
+//var user;
+//
+//function fetchUser() {
+//    fetch('/user')
+//        .then(function (response) {
+//            return response.json();
+//        })
+//        .then(function (jsonData) {
+//            user = jsonData;
+//
+//        });
+//}
 var user;
+fetchUser();
 
 function fetchUser() {
-    fetch('/user')
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (jsonData) {
-            user = jsonData;
-        });
+  fetch('/user')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (jsonData) {
+      console.log(jsonData);
+      var userRole = jsonData.userRole;
+      if (userRole == "ROLE_ADMIN") {
+        console.log("Admin");
+      } else if (userRole == "ROLE_USER") {
+        console.log("User");
+      } else {
+        console.log("anyad");
+      }
+      user = jsonData;
+
+    });
 }
