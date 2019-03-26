db6638b7816586d57ca43de839679747561bdced
csorbarenata <csorba.renata@gmail.com>
2019-03-26 18:48:59 +0100

    OrderDaoTest 2.fele kész

--- src/test/java/training360/j5webshop/order/OrderDaoTest.java
+++ src/test/java/training360/j5webshop/order/OrderDaoTest.java
@@ -11,10 +11,7 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import training360.j5webshop.baskets.Basket;
 import training360.j5webshop.baskets.BasketDao;
-import training360.j5webshop.orders.Order;
-import training360.j5webshop.orders.OrderDao;
-import training360.j5webshop.orders.OrderService;
-import training360.j5webshop.orders.OrderedProduct;
+import training360.j5webshop.orders.*;
 import training360.j5webshop.products.Product;
 import training360.j5webshop.products.ProductDao;
 import training360.j5webshop.users.User;
@@ -37,6 +34,7 @@ public class OrderDaoTest {
     @Autowired
     private ProductDao productDao;
 
+
     @Test
     public void addOrderedPorductTest(){
         Basket basket = basketDao.findBasket(2L);
@@ -48,6 +46,89 @@ public class OrderDaoTest {
         assertThat(orderedProducts.get(0).getName(), equalTo(new OrderedProduct(productDao.findProductById(2), 1).getName()));
     }
 
+    @Test
+    public void testListAllOrderWithDeleted() {
+        //Given
+        orderDao.createOrder(2L);
+        orderDao.addOrderedProduct(2L, basketDao.findBasket(2L));
+        orderDao.deleteWholeOrder(2L);
+        //When
+        List<Order> list = orderDao.listAllOrderWithDeleted("nagygizi22");
+        //Then
+        assertThat(list.size(), equalTo(2));
+    }
 
 
+    @Test
+    public void testListAdminOrders() {
+        //When
+        List<OrderInfo> list = orderDao.listAdminOrders();
+        //Then
+        assertThat(list.size(), equalTo(2));
+    }
+
+
+    @Test
+    public void testListActiveAdminOrders() {
+        //Given
+        orderDao.addOrderedProduct(2L, basketDao.findBasket(2L));
+        //When
+        List<OrderInfo> list = orderDao.listActiveAdminOrders();
+        //Then
+        assertThat(list.size(), equalTo(1));
+    }
+
+    @Test
+    public void testDeleteItem() {
+        //Given
+        orderDao.addOrderedProduct(2L, basketDao.findBasket(2L));
+        //When
+        List<OrderedProduct> listBeforeDelete = orderDao.findOrderedProductByOrderId(2);
+        orderDao.deleteItem(2L, "dixit");
+        List<OrderedProduct> listAfterDelete = orderDao.findOrderedProductByOrderId(2);
+        //Then
+        assertThat(listBeforeDelete.size(), equalTo(3));
+        assertThat(listAfterDelete.size(), equalTo(2));
+    }
+
+    @Test
+    public void testDeleteWholeOrder() {
+        //Given
+        orderDao.addOrderedProduct(2L, basketDao.findBasket(2L));
+        //When
+        List<OrderedProduct> listBeforeDelete = orderDao.findOrderedProductByOrderId(2);
+        orderDao.deleteWholeOrder(2L);
+        List<OrderedProduct> listAfterDelete = orderDao.findOrderedProductByOrderId(2);
+        //Then
+        assertThat(listBeforeDelete.size(), equalTo(3));
+        assertThat(listAfterDelete.size(), equalTo(0));
+    }
+
+    public void testTotalPrice() {
+        //Given
+        orderDao.addOrderedProduct(2L, basketDao.findBasket(2L));
+        //When
+        int totalPrice = orderDao.totalPrice(2L);
+        //Then
+        assertThat(totalPrice, equalTo(61970));
+    }
+
+    @Test
+    public void testFindOrderedProductByOrderId() {
+        //Given
+        orderDao.addOrderedProduct(2L, basketDao.findBasket(2L));
+        //When
+        List<OrderedProduct> list = orderDao.findOrderedProductByOrderId(2);
+        //Then
+        assertThat(list.size(), equalTo(3));
+    }
+
+
+    @Test
+    public void testChangeStatuseByIdAndListAllOrder() {
+         //When
+        orderDao.changeStatusById(2);
+        //Then
+        assertThat(orderDao.listAllOrder("tadri1988").get(0).getOrderStatus(), equalTo(OrderStatus.DELIVERED));
+    }
 }
