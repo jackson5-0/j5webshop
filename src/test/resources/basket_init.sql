cf0ac057add2e01d7f7ec8c8aa3c201eb5b44d2b
Kerklecz Médea <kerklecz.medea@gmail.com>
2019-03-26 10:27:12 +0100

    init.sql test - id felvitele

--- src/test/resources/basket_init.sql
+++ src/test/resources/basket_init.sql
@@ -1,1 +1,12 @@
 delete from basket_item
+
+insert into basket_item(id, basket_id, product_id) values
+    (1, 2, 3);
+insert into basket_item(id, basket_id, product_id) values
+    (2, 2, 4);
+insert into basket_item(id, basket_id, product_id) values
+    (3, 2, 1);
+insert into basket_item(id, basket_id, product_id) values
+    (4, 3, 3);
+insert into basket_item(id, basket_id, product_id) values
+    (5, 3, 4);
